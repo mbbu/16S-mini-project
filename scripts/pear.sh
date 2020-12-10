c48129f5643dd2377d383bd97d884568802905ae
virginiah894 <virgyperry@gmail.com>
2020-12-10 17:01:25 +0300

    adds third workflow

--- scripts/pear.sh
+++ scripts/pear.sh
@@ -1,5 +1,4 @@
 #! usr/bin/env/bash
-
 #Do FastQc
 fastqc *.fastq
 mkdir fastqc_results
@@ -9,31 +8,29 @@ for zip in *.zip; do unzip $zip; done
 cat */summary.txt >> fastqc_summary.txt
 mv fastqc_summary.txt ../
 cd ..
-#Do Trimmomatic for trimming
+#Do Trimming with Prinseq
 for i in `ls -1 *R1*.fastq | sed 's/\_R1.fastq//'`
 do
-trimmomatic PE -phred33 $i\_R1.fastq $i\_R2.fastq $i\_R1_paired.fastq $i\_R1_unpaired.fastq $i\_R2_paired.fastq $i\_R2_unpaired.fastq LEADING:3  TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+prinseq-lite.pl -fastq $i\_R1.fastq -fastq2 $i\_R2.fastq -out_format 3
 done
-
-mkdir trimmomatic_results
-mv *paired* trimmomatic_results/
-cd trimmomatic_results/
-
-#Do quality check for trimmomatic results
-fastqc *_paired.fastq
-mkdir trimmomatic_fastqc_results
-mv *.zip trimmomatic_fastqc_results && mv *.html trimmomatic_fastqc_results
-cd trimmomatic_fastqc_results
+mkdir prinseq_results
+mv *good* prinseq_results/
+cd prinseq_results/
+#Do quality check for prinseq results
+fastqc *good*
+mkdir prinseq_fastqc_results
+mv *.zip prinseq_fastqc_results && mv *.html prinseq_fastqc_results
+cd prinseq_fastqc_results
 for zip in *.zip; do unzip $zip; done
-cat */summary.txt >> trimmomatic_fastqc_summary.txt
-mv trimmomatic_fastqc_summary.txt ../../
+cat */summary.txt >> prinseq_fastqc_summary.txt
+mv prinseq_fastqc_summary.txt ../../
 cd ..
-
-for file in *R1_paired.fastq 
-do 
-pear -f ${file%%_*}_R1_paired.fastq -r ${file%%_*}_R2_paired.fastq -o pairedouputs;
+#Merge the prinseq reads with pear
+for file in *R1_prinseq*
+do
+pear -f ${file%%_*}_R1_prinseq* -r ${file%%_*}_R2_prinseq* -o pairedouputs
 done
 
+#Remove Chimeras with chimeraslayer
 
-
-# # for file in *R1_paired.fastq; do echo "${r%%_*}_R1_paired.fastq ${r%%_*}_R2_paired.fastq"; done
+chimeraslayer --query_NAST pairedouputs.assembled.fastq
